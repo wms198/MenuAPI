71d2fe3fa1bbc36bc10e7e4e9e2f61f462f5ef97
wms198 <wang.minshi@icloud.com>
2025-01-25 10:13:37 +0100

    created postgresdb

---
+++ postgresdb/postgres_repo.go
@@ -0,0 +1,154 @@
+package postgresdb
+
+import (
+	"errors"
+	"fmt"
+	"gorestserviceagain/entity"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var Postgres *gorm.DB
+
+type PostgresDB struct {
+	db *gorm.DB
+}
+
+func newConnection(dsn string) error {
+	if Postgres != nil {
+		return nil
+	}
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn),
+	})
+	if err != nil {
+		return entity.ErrDBNotConnected
+	}
+	Postgres = db
+	return nil
+}
+
+func NewPostgres(dsn string) (PostgresDB, error) {
+	var r PostgresDB
+	err := newConnection(dsn)
+	if err != nil {
+		return r, err
+	}
+	r.db = Postgres
+	return r, nil
+}
+
+func (r PostgresDB) Migrate() error {
+	return errors.Join(
+		r.db.AutoMigrate(&entity.Order{}, &entity.Dish{}, &entity.DiscountDetail{}),
+		errors.New("error migrating db schema"),
+	)
+}
+
+func (r PostgresDB) CreateOrder(order *entity.Order) error {
+	result := r.db.Create(&order)
+	if errors.Is(result.Error, gorm.ErrInvalidData) {
+		return fmt.Errorf("%w:%w", entity.ErrInvalidData, result.Error)
+	}
+	return nil
+}
+
+func (r PostgresDB) GetOrders() (o []entity.Order, err error) {
+	result := r.db.Find(&o)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, fmt.Errorf("%w:%w", errors.New("oders not found"), result.Error)
+	}
+	return o, nil
+}
+
+func (r PostgresDB) GetOrder(id uint) (o entity.Order, err error) {
+	o.ID = id
+	result := r.db.First(&o, o.ID)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return o, entity.WrapRecordNotFoundError("Order", id, result.Error)
+	}
+	return o, nil
+}
+func (r PostgresDB) UpdateOrder(o *entity.Order) error {
+	result := r.db.Model(o).Updates(*o)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return entity.WrapRecordNotFoundError("Order", o.ID, result.Error)
+	}
+	return nil
+}
+
+func (r PostgresDB) UpdateDiscount(d *entity.DiscountDetail) error {
+	result := r.db.Model(d).Updates(*d)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return fmt.Errorf("OrderId or dishId can not be found, can not update")
+	}
+	return nil
+}
+func (r PostgresDB) DeleteOrder(id uint) error {
+	var o entity.Order
+	result := r.db.Delete(&o, id)
+	if result.RowsAffected == 0 {
+		return entity.WrapRecordNotFoundError("Order", id, result.Error)
+	}
+	return nil
+}
+
+func (r PostgresDB) CreateDish(dish *entity.Dish) error {
+	result := r.db.Create(&dish)
+	if errors.Is(result.Error, gorm.ErrInvalidData) {
+		return fmt.Errorf("%w:%w", entity.ErrInvalidData, result.Error)
+	}
+	return nil
+}
+
+func (r PostgresDB) GetDishes() (d []entity.Dish, err error) {
+	result := r.db.Find(&d)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return d, fmt.Errorf("%w:%w", entity.ErrRecordNotFound, result.Error)
+	}
+	return d, nil
+}
+
+func (r PostgresDB) GetDish(id uint) (d entity.Dish, err error) {
+	d.ID = id
+	result := r.db.First(&d, d.ID)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return d, entity.WrapRecordNotFoundError("Dish", id, result.Error)
+	}
+	return d, nil
+}
+
+func (r PostgresDB) UpdateDish(dish *entity.Dish) error {
+	result := r.db.Model(dish).Updates(*dish)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return entity.WrapRecordNotFoundError("Dish", dish.ID, result.Error)
+	}
+	return nil
+}
+
+func (r PostgresDB) DeleteDish(id uint) error {
+	var dish entity.Dish
+	result := r.db.Delete(&dish, id)
+	if result.RowsAffected == 0 {
+		return entity.WrapRecordNotFoundError("Dish", id, result.Error)
+	}
+	return nil
+}
+
+func (r PostgresDB) CreateDiscount(price *entity.DiscountDetail) error {
+	result := r.db.Create(&price)
+	if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+		return fmt.Errorf("%s:%w", "the discount price with the same orderId and dishIs has existed", result.Error)
+	}
+	return nil
+}
+
+func (r PostgresDB) GetPriceAfterDiscount(orderId uint, dishId uint) (discountDetail entity.DiscountDetail, err error) {
+	result := r.db.Joins("Dish").Joins("Order").Where(&discountDetail).First(&discountDetail)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return discountDetail, fmt.Errorf("orderId or itemId does not exist, just ne delete")
+	}
+	return discountDetail, nil
+}
